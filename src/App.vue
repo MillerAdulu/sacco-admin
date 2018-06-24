efb32d434e05810b558da2274c3c07968923cd6c
Miller Adulu <milleradulu@gmail.com>
2018-06-24 23:11:20 +0300

    Added collect.js together with a MemberRegistration file

--- src/App.vue
+++ src/App.vue
@@ -7,6 +7,8 @@
         <v-container fluid>
           <router-view></router-view>
         </v-container>
+
+
       </v-content>
       <v-footer app></v-footer>
     </v-app>
@@ -14,7 +16,7 @@
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HelloWorld from './components/MemberRegistration.vue'
 
 export default {
   name: 'app',
@@ -23,14 +25,3 @@ export default {
   }
 }
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>

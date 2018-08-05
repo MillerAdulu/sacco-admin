e10161cdec86d4092c3a9426c290d279c6c34831
Miller Adulu <milleradulu@gmail.com>
2018-08-06 00:52:40 +0300

    View member contributions

--- src/components/Contributions/MemberContributions.vue
+++ src/components/Contributions/MemberContributions.vue
@@ -1,36 +1,47 @@
 <template>
-<v-data-iterator
-:items="contributions"
-:rows-per-page-items="rowsPerPageItems"
-content-tag="v-layout"
-row
-wrap
->
-<v-flex
-slot="item"
-slot-scope="props"
-xs6
->
-<v-card>
-  <v-card-title><h4>Contribution {{ props.item.memberContributionId}}</h4></v-card-title>
-  <v-divider />
-  <v-list dense>
-    <v-list-tile>
-      <v-list-tile-content>Payment Method:</v-list-tile-content>
-      <v-list-tile-content class="align-end">{{ props.item.paymentMethod.paymentMethod }}</v-list-tile-content>
-    </v-list-tile>
-    <v-list-tile>
-      <v-list-tile-content>Contribution Amount:</v-list-tile-content>
-      <v-list-tile-content class="align-end">{{ props.item.contributionAmount }}</v-list-tile-content>
-    </v-list-tile>
-    <v-list-tile>
-      <v-list-tile-content>Date:</v-list-tile-content>
-      <v-list-tile-content class="align-end">{{ moment(props.item.createdAt).format('MMMM Do YYYY, h:mm:ss a') }}</v-list-tile-content>
-    </v-list-tile>
-  </v-list>
-</v-card>
-</v-flex>
-</v-data-iterator>
+<v-layout>
+  <!-- <v-layout row>
+  <la-cartesian :data="contributions" :width="width" :height="height">
+    <la-line dot curve prop="contributionAmount" />
+    <la-x-axis prop="createdAt"/>
+    <la-tooltip />
+  </la-cartesian>
+  </v-layout> -->
+  <v-layout row>
+    <v-data-iterator
+    :items="contributions"
+    :rows-per-page-items="rowsPerPageItems"
+    content-tag="v-layout"
+    row
+    wrap
+    >
+    <v-flex
+    slot="item"
+    slot-scope="props"
+    xs6
+    >
+    <v-card>
+      <v-card-title><h4>Contribution {{ props.item.memberContributionId}}</h4></v-card-title>
+      <v-divider />
+      <v-list dense>
+        <v-list-tile>
+          <v-list-tile-content>Payment Method:</v-list-tile-content>
+          <v-list-tile-content class="align-end">{{ props.item.paymentMethod.paymentMethod }}</v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-content>Contribution Amount:</v-list-tile-content>
+          <v-list-tile-content class="align-end">{{ props.item.contributionAmount }}</v-list-tile-content>
+        </v-list-tile>
+        <v-list-tile>
+          <v-list-tile-content>Date:</v-list-tile-content>
+          <v-list-tile-content class="align-end">{{ moment(props.item.createdAt).format('MMMM Do YYYY, h:mm:ss a') }}</v-list-tile-content>
+        </v-list-tile>
+      </v-list>
+    </v-card>
+    </v-flex>
+    </v-data-iterator>
+  </v-layout>
+</v-layout>
 </template>
 <script>
 import moment from 'moment'
@@ -38,11 +49,13 @@ import moment from 'moment'
 export default {
   data() {
     return {
+      width: 600,
+      height: 300,
       moment,
       rowsPerPageItems: [4, 8, 12],
       pagination: {
         rowsPerPage: 3
-      }
+      },
     };
   },
   props: {

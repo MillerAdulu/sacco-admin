41802ce03e5c40810591c0cc58866f0ab4518b14
Miller Adulu <milleradulu@gmail.com>
2018-08-28 23:27:36 +0300

    Use online api

--- src/config.js
+++ src/config.js
@@ -9,8 +9,8 @@ let backoff = new ExponentialBackoff({
 })
 
 const HTTP = axios.create({
-  baseURL: `http://localhost:8000/api`,
-  // baseURL: `https://sedcapi.herokuapp.com/api/`,
+  // baseURL: `http://localhost:8000/api`,
+  baseURL: `https://sedcapi.herokuapp.com/api/`,
   headers: {
     'Content-Type': 'application/x-www-form-urlencoded',
     'Accept': 'application/json',

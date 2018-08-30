6d7d00cecf54d3c7be0ec8b67111686f467320a4
Miller Adulu <milleradulu@gmail.com>
2018-08-30 23:16:12 +0300

    Added express server to serve static resources

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
+require(`dotenv`).config()
+
+const app = express()
+
+app.use(
+  serveStatic(
+    path.join(__dirname, `dist`)
+  )
+)
+
+const port = process.env.APP_PORT || 80
+
+app.listen(port, function() {
+  console.log(`Sacco app is running`)
+})

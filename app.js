cf77724392108d3b8b77f0ed3f00691ed8a33ec8
Miller Adulu <milleradulu@gmail.com>
2018-08-31 14:43:40 +0300

    Serve static files and redirect all other routes to index.html

--- app.js
+++ app.js
@@ -5,14 +5,19 @@ require(`dotenv`).config()
 
 const app = express()
 
-app.use(
-  serveStatic(
-    path.join(__dirname, '/dist')
-  )
+app.use(`/`, serveStatic(
+  path.join(__dirname, '/dist')
 )
+)
+
+app.get(`*`, function(req, res) {
+  res.sendFile(
+    path.join(__dirname, `/dist/index.html`)
+  )
+})
 
 const port = process.env.APP_PORT || 5000
 
-app.listen(port, function() {
-  console.log(`Sacco app is running`)
+app.listen(port, function () {
+  console.log(`Sacco app is running on port ${port}`)
 })

a49ec0bf85ea1c1a838f1612295257b399890608
Miller Adulu <milleradulu@gmail.com>
2018-09-03 22:46:28 +0300

    Deploy to heroku changes

--- app.js
+++ app.js
@@ -1,23 +1,17 @@
 const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+const port = process.env.APP_PORT || 5000
 require(`dotenv`).config()
 
 const app = express()
 
-app.use(`/`, serveStatic(
-  path.join(__dirname, '/dist')
-)
-)
+app.use(express.static(
+  __dirname + `/dist/`
+))
 
-app.get(`*`, function (req, res) {
+app.get(/.*/, function (req, res) {
   res.sendFile(
-    path.join(__dirname, `/dist/index.html`)
-  )
+    __dirname + `/dist/index.html`
+  );
 })
 
-const port = process.env.APP_PORT || 5000
-
-app.listen(port, function () {
-  console.log(`Sacco app is running on port ${port}`)
-})
+app.listen(port)

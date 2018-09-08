6c2aef2dc453ef5309c86843be236f9ed4cb364f
Miller Adulu <milleradulu@gmail.com>
2018-09-08 13:33:30 +0300

    Heroku Deploy config

--- app.js
+++ app.js
@@ -1,12 +1,14 @@
 const express = require('express')
+const serveStatic = require('serve-static')
+const path = require('path')
 const port = process.env.APP_PORT || 5000
 require(`dotenv`).config()
 
 const app = express()
 
-app.use(express.static(
+app.use('/', serveStatic(path.join(
   __dirname + `/dist/`
-))
+)))
 
 app.get(/.*/, function (req, res) {
   res.sendFile(

5d17151b43f0c993a5cc18a653b97e7cc49ac9b7
Miller Adulu <milleradulu@gmail.com>
2018-09-09 22:20:32 +0300

    connect history server

--- app.js
+++ app.js
@@ -1,20 +1,11 @@
 const express = require('express')
-const serveStatic = require('serve-static')
-const path = require('path')
+const history = require('connect-history-api-fallback')
+
 const port = process.env.APP_PORT || 5000
 require(`dotenv`).config()
 
 const app = express()
 
-app.use('/', serveStatic(
-  path.join(__dirname + `/dist/`)
-  )
-)
-
-app.get(/.*/, function (req, res) {
-  res.sendFile(
-    __dirname + `/dist/index.html`
-  );
-})
+app.use(history())
 
 app.listen(port)

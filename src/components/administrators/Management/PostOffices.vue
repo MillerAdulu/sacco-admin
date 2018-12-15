fde62452397984561bc6a526dddeaa6f44d7a353
Miller Adulu <milleradulu@gmail.com>
2018-12-15 16:13:15 +0300

    Update post offices

--- src/components/administrators/Management/PostOffices.vue
+++ src/components/administrators/Management/PostOffices.vue
@@ -1,80 +1,159 @@
 <template>
   <v-container>
     <v-card>
-      <v-card-title>
-        All Post Offices
+      <v-card-title>All Post Offices
         <v-spacer></v-spacer>
         <v-text-field
-            v-model="search"
-            append-icon="search"
-            label="Search"
-            single-line
-            hide-details
+          v-model="search"
+          append-icon="search"
+          label="Search"
+          single-line
+          hide-details
         />
       </v-card-title>
       <v-data-table
-          :headers="headers"
-          :items="postOffices"
-          :loading="dataLoading"
-          :search="search"
-          hide-actions
-          class="elevation-1"
+        :headers="headers"
+        :items="postOffices"
+        :loading="dataLoading"
+        :search="search"
+        hide-actions
+        class="elevation-1"
       >
-        <v-progress-linear slot="progress" color="blue" indeterminate />
+        <v-progress-linear slot="progress" color="blue" indeterminate/>
 
         <template slot="items" slot-scope="props">
           <td class="text-xs-left">{{ props.item.postOfficeId }}</td>
-          <td class="text-xs-left">{{ props.item.postOfficeCode }}</td>
-          <td class="text-xs-left">{{ props.item.postOfficeName }}</td>
-          <td class="text-xs-left">{{ moment(props.item.createdAt).format('MMMM Do YYYY, h:mm:ss a') }}</td>
+          <td class="text-xs-left">
+            <v-edit-dialog
+              :return-value.sync="props.item.postOfficeCode"
+              lazy
+              @save="save(props.item)"
+              @cancel="cancel"
+            >
+              {{ props.item.postOfficeCode }}
+              <v-text-field
+                slot="input"
+                v-model="props.item.postOfficeCode"
+                :rules="[max15chars]"
+                label="Edit"
+                single-line
+                counter
+              ></v-text-field>
+            </v-edit-dialog>
+          </td>
+          <td class="text-xs-left">
+            <v-edit-dialog
+              :return-value.sync="props.item.postOfficeName"
+              lazy
+              @save="save(props.item)"
+              @cancel="cancel"
+            >
+              {{ props.item.postOfficeName }}
+              <v-text-field
+                slot="input"
+                v-model="props.item.postOfficeName"
+                :rules="[max15chars]"
+                label="Edit"
+                single-line
+                counter
+              ></v-text-field>
+            </v-edit-dialog>
+            </td>
+          <td
+            class="text-xs-left"
+          >{{ moment(props.item.createdAt).format('MMMM Do YYYY, h:mm:ss a') }}</td>
         </template>
       </v-data-table>
     </v-card>
-    <base-snackbar />
+    <base-snackbar/>
   </v-container>
 </template>
 
 <script>
-  import moment from "moment";
-  import postofficesMixin from '@/components/administrators/mixins/postoffices'
+import moment from "moment";
+import postofficesMixin from "@/components/administrators/mixins/postoffices";
+import SaccoAPI from "@/api";
+import queryString from "querystring";
+import Parsers from "@/helpers/parsers";
+import bugsnagClient from "@/helpers/errorreporting";
 
-  export default {
-    name: `PostOfficeList`,
-    data() {
-      return {
-        moment,
-        dataLoading: true,
-        search: ``,
-        headers: [
-          {
-            text: "Post Office ID",
-            value: "postOfficeId",
-          },
-          {
-            text: "Post Office Code",
-            value: "postOfficeCode",
-          },
-          {
-            text: "Post Office Name",
-            value: "postOfficeName",
-          },
-          {
-            text: "Added On",
-            value: "createdAt",
-          },
-        ]
-      };
-    },
-    mixins: [
-      postofficesMixin,
-    ],
-    methods: {
-      stopLoading() {
-        this.dataLoading = false
+export default {
+  name: `PostOfficeList`,
+  data() {
+    return {
+      moment,
+      dataLoading: true,
+      search: ``,
+      max15chars: v => v.length <= 15 || "Input too long!",
+      headers: [
+        {
+          text: "Post Office ID",
+          value: "postOfficeId"
+        },
+        {
+          text: "Post Office Code",
+          value: "postOfficeCode"
+        },
+        {
+          text: "Post Office Name",
+          value: "postOfficeName"
+        },
+        {
+          text: "Added On",
+          value: "createdAt"
+        }
+      ]
+    };
+  },
+  mixins: [postofficesMixin],
+  methods: {
+    save(postOffice) {
+      if (this.$can(`update`, `PostOffice`)) {
+        let updateData = Parsers.prepareDataObject({
+          post_office_code: postOffice.postOfficeCode,
+          post_office_name: postOffice.postOfficeName,
+        });
+        
+        SaccoAPI.put(
+          `/postoffices/${postOffice.postOfficeId}`,
+          queryString.stringify(updateData)
+        )
+          .then(() => {
+            this.$store.commit(`setSnackbar`, {
+              msg: `Post Office updated!`,
+              type: `success`,
+              model: true
+            });
+          })
+          .catch(error => {
+            bugsnagClient.notify(error);
+            this.$store.commit(`setSnackbar`, {
+              msg: `Failed to update post office!`,
+              type: `error`,
+              model: true
+            });
+          });
+      } else {
+        this.$store.commit(`setSnackbar`, {
+          msg: `You don't have permissions to edit post offices`,
+          type: `error`,
+          model: true
+        });
       }
     },
-    created() {
-      this.getPostOffices()
+    cancel() {
+      this.$store.commit(`setSnackbar`, {
+        msg: `Aborted`,
+        type: `error`,
+        model: true
+      });
+    },
+    stopLoading() {
+      this.dataLoading = false;
     }
-  };
+  },
+  created() {
+    this.getPostOffices();
+  }
+};
 </script>
